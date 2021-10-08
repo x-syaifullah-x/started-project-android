e6a3b6fb27ec6657479d1e9608519f974a10f993
x-syaifullah-x <x.syaifullah.x@gmail.com>
2021-10-08 22:22:04 +0700

    ...

---
+++ app/src/androidTest/java/id/xxx/example/ExampleInstrumentTest.kt
@@ -0,0 +1,11 @@
+package id.xxx.example
+
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Test
+
+class ExampleInstrumentTest {
+    @Test
+    fun exampleInstrumentTest() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+    }
+}

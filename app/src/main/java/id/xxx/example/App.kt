40b090dad2d022d012675bc33a361f3e0a2a2583
x-syaifullah-x <x.syaifullah.x@gmail.com>
2021-11-12 09:22:50 +0700

    ...

--- app/src/main/java/id/xxx/example/App.kt
+++ app/src/main/java/id/xxx/example/App.kt
@@ -1,12 +1,9 @@
 package id.xxx.example
 
 import android.app.Application
-import id.xxx.module.common.helper.DataHelper
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-
-
     }
 }
